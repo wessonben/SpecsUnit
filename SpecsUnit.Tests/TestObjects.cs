98346590bfcef6d0f8508b6c9441d2b3abf0e2f7
Ben Wesson <35484194+wessonben@users.noreply.github.com>
2018-03-23 14:47:05 +0000

    Add files via upload

---
+++ SpecsUnit.Tests/TestObjects.cs
@@ -0,0 +1,19 @@
+﻿namespace SpecsUnit.Tests
+{
+    public interface ITestDependency
+    {
+        string GetString();
+    }
+
+    public class TestObject
+    {
+        public ITestDependency Dependency { get; }
+
+        public TestObject(ITestDependency dependency)
+        {
+            Dependency = dependency;
+        }
+
+        public string GetString() => Dependency.GetString();
+    }
+}
---
+++ SpecsUnit/Spec.cs
@@ -0,0 +1,64 @@
+﻿using Moq;
+using Moq.AutoMock;
+using NUnit.Framework;
+
+namespace SpecsUnit
+{
+
+
+    public interface ISpec<out T> 
+    {
+        T SUT { get; }
+
+        Mock<TMock> GetMock<TMock>() where TMock : class;
+    }
+
+
+    [TestFixture]
+    public abstract class Spec<T> : ISpec<T> where T : class
+    {
+        public T SUT { get; private set; }
+
+        private readonly AutoMocker _autoMocker;
+
+
+        protected Spec()
+        {
+            _autoMocker = new AutoMocker();
+        }
+
+
+        [OneTimeSetUp]
+        public void SetupTests()
+        {
+            InitialiseSUT();
+            Given();
+            When();
+        }
+
+
+        public Mock<TMock> GetMock<TMock>() where TMock : class => 
+            _autoMocker.GetMock<TMock>();
+
+
+        protected virtual void InitialiseSUT()
+        {
+            SUT = _autoMocker.CreateInstance<T>();
+        }
+
+
+        protected virtual void Given()
+        {
+            
+        }
+
+        protected virtual void When()
+        {
+            
+        }
+
+
+
+
+    }
+}
